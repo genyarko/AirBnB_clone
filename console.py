5577bfc08f9686f9ac2baf6cfb86b25a987a9de3
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-15 03:13:13 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,46 +1,26 @@
 #!/usr/bin/env python3
-"""This module contains the HBNBCommand class for the console"""
+"""Console module."""
 
 import cmd
-import re
-from shlex import split
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for the HBNB console"""
+    """HBNB command interpreter."""
 
-    prompt = "(hbnb) "
-    __classes = {
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Place",
-        "Amenity",
-        "Review"
-    }
-
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
-        print("")
+        """EOF command to exit the program."""
+        print()
         return True
 
     def emptyline(self):
-        """Do nothing on empty input line"""
+        """Do nothing on empty line + ENTER."""
         pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
