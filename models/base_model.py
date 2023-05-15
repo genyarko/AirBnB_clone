7fdd79e34c825cf96376c4e2775345f7296ee865
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-15 02:22:38 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,25 +6,33 @@ Contains the Base class for the AirBnB clone console.
 import uuid
 from datetime import datetime
 from models import storage
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
+
 
 
 class BaseModel:
     """Base class for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize the attributes of the BaseModel class"""
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key in ["created_at", "updated_at"]:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            """
+    Initializes a new instance of BaseModel.
+    If kwargs is not empty, sets instance attributes based on key/value pairs
+    in kwargs. Otherwise, initializes new instance with unique id and created_at.
+    """
+    if kwargs:
+        for k, v in kwargs.items():
+            if k == "__class__":
+                continue
+            if k == "created_at" or k == "updated_at":
+                v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+            setattr(self, k, v)
+    else:
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         """Return the string representation of the BaseModel instance"""
@@ -32,9 +40,13 @@ class BaseModel:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the public instance attribute updated_at with the current datetime"""
+    """
+    Updates the public instance attribute updated_at with the current
+    datetime and saves the instance to the storage.
+    """
+    self.updated_at = datetime.now()
+    storage.save()
 
-        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of __dict__"""
