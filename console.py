7804287884fa6c8bcf5ebfc87e8671cf0909bae0
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-15 03:18:23 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,27 +1,150 @@
-#!/usr/bin/env python3
-"""Console module."""
+#!/usr/bin/python3
+"""
+Command interpreter for HBNB project
+"""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """HBNB command interpreter."""
+    """
+    Command interpreter class
+    """
 
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """
+        Quit command to exit the program
+        """
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program."""
+        """
+        Exit program with EOF (Ctrl-D)
+        """
         print()
         return True
 
     def emptyline(self):
-        """Do nothing on empty line + ENTER."""
+        """
+        Do nothing on empty line + ENTER
+        """
         pass
 
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        cls_name = args[0]
+
+        if cls_name not in storage.classes.keys():
+            print("** class doesn't exist **")
+            return
+
+        new_instance = storage.classes[cls_name]()
+
+        new_instance.save()
+
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        cls_name = args[0]
+
+        if cls_name not in storage.classes.keys():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+
+        inst_id = args[1]
+        key = "{}.{}".format(cls_name, inst_id)
+
+        if key not in storage.all().keys():
+            print("** no instance found **")
+            return
+
+        print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (save the change into the JSON file)
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        cls_name = args[0]
+
+        if cls_name not in storage.classes.keys():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+
+        inst_id = args[1]
+        key = "{}.{}".format(cls_name, inst_id)
+
+        if key not in storage.all().keys():
+            print("** no instance found **")
+            return
+
+        storage.delete(key)
+        storage.save()
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name
+        """
+        if not arg:
+            print([str(val) for val in storage.all().values()])
+            return
+
+        args = arg.split()
+        cls_name = args[0]
+
+        if cls_name not in storage.classes.keys():
+            print("** class doesn't exist **")
+            return
+
+        print([str(val) for key, val in storage.all().items() if key.startswith(cls_name + '.')])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        cls_name = args[0]
+
+        if cls_name not in storage.classes.keys():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
 
