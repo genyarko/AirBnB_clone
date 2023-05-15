b0ec36119bdca8d2b84bf36355b4353a209ef229
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-15 02:53:55 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,19 +6,17 @@ import os
 
 
 class FileStorage:
-
     """Class for serializtion and deserialization of base classes."""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns __objects dictionary."""
-        # TODO: should this be a copy()?
         return FileStorage.__objects
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
-        # TODO: should these be more precise specifiers?
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
@@ -55,7 +53,6 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
@@ -95,3 +92,6 @@ class FileStorage:
                          "text": str}
         }
         return attributes
+
+storage = FileStorage()
+storage.reload()
